7b2ef7ca2646d70879b8918e8bf1f6203a233b3a
KeyZxy <118372186+KeyZxy@users.noreply.github.com>
2024-07-10 23:25:19 +0800

    change outlook

--- Quidditch/Assets/Quidditch/Player1/scripts/b2.cs
+++ Quidditch/Assets/Quidditch/Player1/scripts/b2.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class b2 : MonoBehaviour
 {
+    public SpriteRenderer sr;//组件
+    public Sprite[] pic;//图片
     // Start is called before the first frame update
     void Start()
     {
-
+        sr = GetComponent<SpriteRenderer>();
         // 示例玩家
         GameObject player1 = GameObject.FindGameObjectWithTag("Player1");
         GameObject player2 = GameObject.FindGameObjectWithTag("Player2");
@@ -16,7 +18,30 @@ public class b2 : MonoBehaviour
         // 抽取一个随机坐骑
         Broom selectedBroom = Broom.ExtractBroom();
         Debug.Log($"玩家2抽取到坐骑：{selectedBroom.broomName}");
+        if (selectedBroom.broomName == "普通扫帚")
+        {
+            sr.sprite = pic[0];
+        }
+        else if (selectedBroom.broomName == "彗星")
+        {
 
+            sr.sprite = pic[1];
+        }
+        else if (selectedBroom.broomName == "光轮2000")
+        {
+
+            sr.sprite = pic[2];
+        }
+        else if (selectedBroom.broomName == "光轮2001")
+        {
+
+            sr.sprite = pic[3];
+        }
+        else if (selectedBroom.broomName == "火弩箭")
+        {
+
+            sr.sprite = pic[4];
+        }
         // 应用坐骑的加成效果
         selectedBroom.ApplyBuff(playerCom2);
 
